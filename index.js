9949e90778629be1495e826c3a4cec58ed337363
sarayoung19 <syoung@medicity.com>
2017-03-02 14:20:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('This is my first exposure to code!')
 })
 
 app.listen(app.get('port'), function() {
